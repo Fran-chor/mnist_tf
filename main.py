f233d392ea38a8e22b07978aacb4a98692ab59e7
Odas-cieux <francois.chorin@gmail.com>
2021-03-24 11:36:32 +0100

    script with arguments

--- main.py
+++ main.py
@@ -1,27 +1,61 @@
+import getopt
+import sys
+
+import mnist_deep.analysis as analysis
 import mnist_deep.mnist as mnist
 import mnist_deep.my_models as my_models
 import mnist_deep.training as training
-import mnist_deep.analysis as analysis
-import tensorflow as tf
-from tensorflow.keras.models import load_model
+
 
 # TODO See the tutorial on github (in my favorites) to improve this code
 # For instance, using repeat in the dataset and doing by step instead of epoch ???
 # TODO See how to do a Notebooks
 
+def usage():
+    print("Usage: {} -m <training/analysis> -e <epochs>".format(sys.argv[0]))
+    print("-g to use the graphs mode (eager_mode by default)")
+    print("-h to print the usage")
+
+
+def get_args():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hm:ge:", ["help", "mode=", "graphs", "epochs="])
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+        sys.exit(2)
+    mode = None
+    graphs = None
+    epochs = None
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif opt in ("-m", "--mode"):
+            if arg in ("training", "analysis"):
+                mode = arg
+            else:
+                usage()
+                sys.exit(2)
+        elif opt in ("-g", "--graphs"):
+            graphs = True
+        elif opt in ("-e", "--epochs"):
+            epochs = int(arg)
+        else:
+            assert False, "unhandled option"
+    return mode, graphs, epochs
+
 
 def main():
 
-    # TODO put these variables in argument
-    epochs = 1
-    eager_mode = True
-    running = "analysis"
+    mode, graphs, epochs = get_args()
+    eager_mode = not graphs
 
     train_ds, test_ds = mnist.load_mnist_dataset()
 
     model = my_models.basic_cnn()
 
-    if running == "training":
+    if mode == "training":
 
         optimizer, loss_fn, train_loss, train_metric, valid_loss, valid_metric = \
             training.initialization(eager_mode)
@@ -29,7 +63,7 @@ def main():
         training.train(model, train_ds, test_ds, optimizer, loss_fn, epochs,
                        train_loss, train_metric, valid_loss, valid_metric)
 
-    elif running == "analysis":
+    elif mode == "analysis":
 
         my_models.load_trained_model(model)
 
